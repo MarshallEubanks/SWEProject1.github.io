9e6e4c622b19147b88bbb3204334b2ad5ad0578d
MarshallEubanks <Marshall.K.Eubanks-1@ou.edu>
2023-11-22 10:14:19 -0600

    Updated login.py to connect front-end to back-end

---
+++ website/login.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pyrebase
+
+app = Flask(__name__)
+
+firebaseConfig = {
+  "apiKey": "AIzaSyCzEocPUOnDcarC0oQu_6_VK-rukqDyEiU",
+  "authDomain": "fridge-42574.firebaseapp.com",
+  "projectId": "fridge-42574",
+  "storageBucket": "fridge-42574.appspot.com",
+  "messagingSenderId": "461258862353",
+  "appId": "1:461258862353:web:a76aa7d322077ef08ee6b0",
+  "measurementId": "G-NHZ6J76QM0",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+
+@app.route('/')
+def index():
+    return render_template('sign_in.html')
+
+@app.route('/signup', methods=['post'])
+def signup():
+      if request.method == 'post':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('registration_success'))
+        except:
+            return "Registration failed"
+
+@app.route('/login', methods=['post'])
+def login():
+   if request.method == 'post':
+      email = request.form['email']
+      password = request.form['password']
+      try:
+         login = auth.sign_in_with_email_and_password(email, password)
+         print("Successfully logged in!")
+      except:
+         print("Invalid email or password")
